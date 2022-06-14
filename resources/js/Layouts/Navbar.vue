326850f18ad13bd6f10a36372f19ea8eb51bd7a6
gokkky <goku_dera_hayato@hotmail.com>
2022-06-14 11:19:21 +0700

    first commit

---
+++ resources/js/Layouts/Navbar.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">BOOK</a>
+
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">สินค้าใหม่</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">สินค้าขายดี</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">สินค้าลดราคา</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">สินค้าแนะนำ</a>
+                        </li>
+                    </ul>
+                    
+                    
+                    <ul class="navbar-nav ml-5">
+                        <li class="nav-item">
+                        <inertia-link :href="$route('cart.index')" class="nav-link">Register</inertia-link>
+                            <!--<a class="nav-link" :href="$route('test')">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                </svg>
+                                <span class="badge badge-notify">3</span>
+                            </a>-->
+                        </li>
+                        <!--<li class="nav-item">
+                            <a class="nav-link" href="#">เข้าสู่ระบบ </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">ลงทะเบียน </a>
+                        </li>-->
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><img src="https://live.staticflickr.com/6202/6046000670_4d5927f3d2_z.jpg" class="avatar" width="30" height="30"> </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        components: {
+        },
+        mounted(){
+        },
+        data() {
+            return {
+                
+            }
+        },
+
+        methods: {
+            
+        },
+
+    }
+</script>
---
+++ resources/js/Pages/Home/Index.vue
@@ -0,0 +1,98 @@
+<template>
+    <div>
+        <Navbar></Navbar>
+        <section>
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-md-4 mb-4">
+                        <div class=" sticky">
+                            <div class="d-flex justify-content-between">
+                                <p class="small">สินค้าขายดี</p>
+                                <p class="small"><a href="#">ดูทั้งหมด</a></p>
+                            </div>
+                            <div class="d-flex mt-3" v-for="index in 4">
+                                <div class="col-md-4">
+                                    <img height="130" src="https://storage.naiin.com/system/application/bookstore/resource/product/201607/196975/6000021934_front_XXL.jpg?imgname=%E0%B8%A3%E0%B8%B1%E0%B8%81%E0%B8%AD%E0%B8%B1%E0%B8%99%E0%B8%95%E0%B8%A3%E0%B8%B2%E0%B8%A2%E0%B8%99%E0%B8%B2%E0%B8%A2%E0%B8%9B%E0%B8%B5%E0%B8%A8%E0%B8%B2%E0%B8%88%E0%B8%84%E0%B8%B8%E0%B9%82%E0%B8%A3%E0%B8%8B%E0%B8%B2%E0%B8%81%E0%B8%B4-1"
+                                        class="card-img-top" alt="Laptop" />
+                                </div>
+                                <div class="col-md-8">
+                                    <div>
+                                        <span>แฟ้มคดีกรมปราบปีศาจ เล่ม 6 (เล่มจบ)</span><br>
+                                        <span class="text-secondary small">เมิ่งซีสือ</span>
+                                    </div>
+
+                                    <div class="d-flex mb-2 small mt-2">
+                                        <div class="ms-auto text-warning">
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                            <i class="fa fa-star"></i>
+                                        </div>
+                                    </div>
+                                    
+                                    <span>250.00 บาท</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="row">
+                            <div class="col-md-12 mb-3 text-center"><h4><b>หนังสือน่าอ่าน ที่ไม่ควรพลาด !!</b></h4></div>
+                            <div v-for="index in 8" class="col-md-3 mb-4 mb-lg-0">
+                                <div class="card" style="border: none;">
+                                    <img height="200" src="https://storage.naiin.com/system/application/bookstore/resource/product/202205/548160/1000248652_front_XXL.jpg?imgname=%E0%B9%81%E0%B8%9F%E0%B9%89%E0%B8%A1%E0%B8%84%E0%B8%94%E0%B8%B5%E0%B8%81%E0%B8%A3%E0%B8%A1%E0%B8%9B%E0%B8%A3%E0%B8%B2%E0%B8%9A%E0%B8%9B%E0%B8%B5%E0%B8%A8%E0%B8%B2%E0%B8%88-%E0%B9%80%E0%B8%A5%E0%B9%88%E0%B8%A1-6-(%E0%B9%80%E0%B8%A5%E0%B9%88%E0%B8%A1%E0%B8%88%E0%B8%9A)"
+                                        class="card-img-top" alt="Laptop" />
+                                    <div class="mt-3">
+
+                                        <div>
+                                            <span>แฟ้มคดีกรมปราบปีศาจ เล่ม 6 (เล่มจบ)</span><br>
+                                            <span class="text-secondary small">เมิ่งซีสือ</span>
+                                        </div>
+
+                                        <div class="d-flex mb-2 small mt-2">
+                                            <div class="ms-auto text-warning">
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                                <i class="fa fa-star"></i>
+                                            </div>
+                                        </div>
+                                        
+                                        <span class="small text-danger"><s>500.25 บาท</s></span>
+                                        <span>
+                                            250.00 บาท
+                                        </span>
+                                        <div class="d-flex flex-row mt-4">
+                                            <button type="button" class="btn btn-info flex-fill ms-1" style="border-radius: 30px;">Add to cart</button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <span class="badge badge-range">{{index}}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+// import {inject, reactive} from "vue";
+// import {Inertia} from "@inertiajs/inertia";
+// import {usePage} from "@inertiajs/inertia-vue3";
+// import Navbar from '@Pages/Home/Navbar'
+import Navbar from "../../Layouts/Navbar";
+
+export default {
+    components: {
+        Navbar
+    },
+    props: {
+        // errors: Object
+    }
+}
+</script>
